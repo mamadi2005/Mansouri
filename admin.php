38c99d04c9775a987ce13d63bb755b662ea59884
mamadi2005 <mhp301832@gmail.com>
2026-06-06 23:40:32 +0330

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,140 @@
+<?php
+include 'db.php';
+date_default_timezone_set('Asia/Tehran');
+session_start();
+
+// چک ورود ادمین
+if(!isset($_SESSION['is_admin_logged']) || $_SESSION['is_admin_logged'] !== true){
+    header("Location: login_admin.php");
+    exit();
+}
+
+if(isset($_GET['delete_id'])){
+    $id = intval($_GET['delete_id']);
+    mysqli_query($conn, "DELETE FROM attendance WHERE id=$id");
+    header("Location: admin.php");
+    exit();
+}
+
+if(isset($_POST['generate_code'])){
+    $adad = random_int(1000,9999);
+    $expire = date("Y-m-d H:i:s", time() + 120); 
+    mysqli_query($conn, "DELETE FROM admin_codes");
+    mysqli_query($conn, "INSERT INTO admin_codes(code, expires_at) VALUES('$adad','$expire')");
+    header("Location: admin.php");
+    exit();
+}
+
+$checkCode = mysqli_query($conn,
+    "SELECT code, expires_at
+     FROM admin_codes
+     WHERE expires_at > NOW()
+     ORDER BY id DESC
+     LIMIT 1"
+);
+
+if(mysqli_num_rows($checkCode) > 0){
+    $row = mysqli_fetch_assoc($checkCode);
+    $result = $row['code'];
+    $expireTime = strtotime($row['expires_at']) * 1000;
+} else {
+    $result = null;
+    $expireTime = 0;
+}
+mysqli_query($conn, "DELETE FROM admin_codes WHERE expires_at <= NOW()");
+$list = mysqli_query($conn, "SELECT * FROM attendance ORDER BY id DESC");
+?>
+<?
+?>
+<?php
+if(isset($_POST['end_term'])){
+    mysqli_query($conn, "DELETE FROM attendance");
+    header("Location: admin.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>پنل استاد</title>
+    <link rel="stylesheet" href="admin.css">
+</head>
+<body>
+    <form method="post" onsubmit="return confirm('مطمئنی میخوای کله لیست حضور ها پاک بشه؟  یادت باشه این کار غیر قابل برگشته (:');">
+    <button type="submit" name="end_term" style="background:red;color:white;padding:10px;border:none;cursor:pointer;">
+        پایان ترم (حذف کل حضورها)
+    </button>
+</form>
+<hr>
+<h1>به پنل استاد خوش آمدید</h1>
+
+<a href="settings.php" class="logout_header">پنل تنظیمات</a>
+<a href="logout_admin.php" class="logout_header">خروج از پنل</a>
+
+<hr>
+
+<h2>ساخت کد ۴ رقمی</h2>
+<form method="post">
+    <button type="submit" name="generate_code">ساخت کد جدید</button>
+</form>
+
+<hr>
+
+<h2>کد فعلی استاد</h2>
+
+<?php if($result): ?>
+    <h1><?php echo $result; ?></h1>
+<?php else: ?>
+    <p>هیچ کدی فعال نیست</p>
+<?php endif; ?>
+
+<div id="timer"></div>
+
+<hr>
+
+<h2>لیست حضور دانشجویان</h2>
+
+<?php
+if(mysqli_num_rows($list) > 0){
+    while($row = mysqli_fetch_assoc($list)){
+        echo "<p>";
+        // echo $row['full_name']." - ".$row['student_code']." - ".$row['created_at'];
+        echo $row['full_name']." - ".$row['student_code']." - ".$row['dars']." - ".$row['created_at'];
+
+        echo " <a href='admin.php?delete_id=".$row['id']."' 
+        onclick=\"return confirm('مطمئنی میخوای این دانشجو حذف بشه از لیست حضور؟');\"
+        style='color:red; margin-right:10px;'>حذف</a>";
+
+        echo "</p>";
+    }
+} else {
+    echo "<p>هنوز حضوری ثبت نشده </p>";
+}
+?>
+
+<script>
+let expireTime = <?php echo $expireTime; ?>;
+
+if(expireTime > 0){
+    let x = setInterval(function(){
+        let now = Date.now();
+        let distance = expireTime - now;
+
+        if(distance <= 0){
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "⛔ کد منقضی شد";
+        } else {
+            let minutes = Math.floor(distance / 60000);
+            let seconds = Math.floor((distance % 60000) / 1000);
+
+            document.getElementById("timer").innerHTML =
+                minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+        }
+    }, 1000);
+}
+</script>
+
+</body>
+</html>
